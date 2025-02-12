02e78f33fa370622b3a26634ea301bd820754852
scottandrewlepera <scott@scottandrew.com>
2025-02-11 17:11:21 -0800

    Support regenerating cookies

--- settings.php
+++ settings.php
@@ -85,22 +85,26 @@ function wp_buttondown_settings_field_callback($args) {
     <tr>
       <th>Regular subscriber cookie name</th>
       <td>
-        <input type="text" name="regular_cookie" value="<?php echo isset( $setting['regular_cookie'] ) ? esc_attr( $setting['regular_cookie'] ) : ''; ?>" required />
+      <?php echo isset( $setting['regular_cookie'] ) ? esc_attr( $setting['regular_cookie'] ) : 'Not set'; ?>
       </td>
     </tr>
     <tr>
       <th>Premium subscriber cookie name</th>
       <td>
-        <input type="text" name="premium_cookie" value="<?php echo isset( $setting['premium_cookie'] ) ? esc_attr( $setting['premium_cookie'] ) : ''; ?>" required />
+        <?php echo isset( $setting['premium_cookie'] ) ? esc_attr( $setting['premium_cookie'] ) : 'Not set'; ?>
       </td>
     </tr>
     <tr>
+      <th>Generate new cookies</th>
+      <td>
+        <input type="checkbox" name="regen_cookies" />
+        <p class="description">This will log out all subscribers, forcing them to log in again.</p>
+      </td>
+    </tr>
   </table>
 
   <h2>Landing page configuration</h2>
   <p>These are the pages that visitors will be redirected to for the login process. You must create these pages yourself, or the plugin can create them for you.</p>
-
-  <p>Note: you must delete existing pages if you want to use the same landing page URLs.</p>
 
   <table class="form-table">
     <tr>
@@ -128,9 +132,10 @@ function wp_buttondown_settings_field_callback($args) {
       </td>
     </tr>
     <tr>
-      <th>Create pages on update?</th>
+      <th>Create pages on update</th>
       <td>
         <input type="checkbox" name="create_pages" />
+        <p class="description">This will create all the pages listed above if they don't already exist.</p>
       </td>
     </tr>
   </table>
@@ -153,36 +158,54 @@ function wp_buttondown_settings_page_html() {
   if ( !current_user_can( 'manage_options' ) ) {
     return;
   }
+
+  $s = wp_buttondown_get_settings();
+
+  $new_opts = array();
+  
   if (isset($_POST['api_token'])) {
 
-    $new_opts = array(
-      'api_token' => sanitize_text_field($_POST['api_token']),
-      'subscribe_page' => sanitize_url($_POST['subscribe_page']),
-      'regular_cookie' => wp_buttondown_sanitize_cookie_name($_POST['regular_cookie']),
-      'premium_cookie' => wp_buttondown_sanitize_cookie_name($_POST['premium_cookie']),
-      'login' => sanitize_text_field($_POST['login']),
-      'success' => sanitize_text_field($_POST['success']),
-      'error' => sanitize_text_field($_POST['error']),
-      'nosub' => sanitize_text_field($_POST['nosub'])
-    );
+    $new_opts['api_token'] = sanitize_text_field($_POST['api_token']);
+    $new_opts['subscribe_page'] = sanitize_url($_POST['subscribe_page']);
+    $new_opts['login'] = sanitize_text_field($_POST['login']);
+    $new_opts['success'] = sanitize_text_field($_POST['success']);
+    $new_opts['error'] = sanitize_text_field($_POST['error']);
+    $new_opts['nosub'] = sanitize_text_field($_POST['nosub']);
+
+    if (isset($_POST['regen_cookies'])) {
+      $new_opts['regular_cookie'] = wp_buttondown_generate_cookie_name();
+      $new_opts['premium_cookie'] = wp_buttondown_generate_cookie_name();
+    } else {
+      $new_opts['regular_cookie'] = $s['regular_cookie'];
+      $new_opts['premium_cookie'] = $s['premium_cookie'];
+    }
+  }
+
+  if ( count($new_opts) !== 0 ) {
 
     $success = update_option('wp_buttondown_settings', $new_opts);
+
     if ($success === true) {
         echo '<div class="updated"><p>Settings updated.</p></div>';
+        if (isset($_POST['regen_cookies'])) {
+          echo '<div class="updated"><p>New cookies generated.</p></div>';
+        }
     } else {
       echo '<div class="error"><p>Update failed.</p></div>';
     }
 
-    if (isset($_POST['create_pages'])) {
-      $page_results = wp_buttondown_settings_create_pages();
-      if ($page_results['code'] == 0) {
-        echo '<div class="updated"><p>Pages created.</p></div>';
-      } else {
-        $page_errors = join('<br />', $page_results['errors']);
-        echo "<div class=\"error\">$page_errors</div>";
-      }
+  }
+
+  if (isset($_POST['create_pages'])) {
+    $page_results = wp_buttondown_settings_create_pages();
+    if ($page_results['code'] == 0) {
+      echo '<div class="updated"><p>Pages created.</p></div>';
+    } else {
+      $page_errors = join('<br />', $page_results['errors']);
+      echo "<div class=\"error\">$page_errors</div>";
     }
   }
+
   ?>
   <div class="wrap">
     <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
@@ -192,7 +215,6 @@ function wp_buttondown_settings_page_html() {
       do_settings_fields( 'wp_buttondown', 'wp_buttondown_settings_section' );
       submit_button( 'Save settings' );
       ?>
-    </form>
   </div>
   <?php
 }
